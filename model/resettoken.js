57dc7b00d81b28fe30c18bbc1a7809a664f93d79
HilaryEmmanuel <emmanuelhilary9@gmail.com>
2023-10-05 23:44:26 +0100

    fix

--- model/resettoken.js
+++ model/resettoken.js
@@ -1,7 +1,8 @@
 const { Sequelize, DataTypes } = require('sequelize');
 require('dotenv').config();
 const sequelize = new Sequelize(`postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, { dialect: "postgres", dialectOptions: { ssl: true }, logging : false })
-sequelize.sync({ force : true})
+// sequelize.sync({ force : true})
+// sequelize.sync()
 const resetToken = sequelize.define('resettoken', {
     token_id: {
         type: DataTypes.BIGINT,
--- model/user.js
+++ model/user.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const sequelize = new Sequelize(`postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, { dialect: "postgres", dialectOptions: { ssl: true, } , logging : false})
 // const sequelize = new Sequelize(`postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, {dialect : "postgres"})
 // sequelize.sync()
+// sequelize.sync({ force : true})
 const users = sequelize.define('users', {
     /* Model Attributes */
     user_id: {
